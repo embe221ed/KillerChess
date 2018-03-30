ece9a8d165c5c56dd6248811e6baabdc2b3e8e8b
embchnk <baniak996@gmail.com>
2018-03-30 16:04:13 +0200

    Add JavaFX in view module

--- core/src/main/java/com/killerchess/core/controllers/app/AppController.java
+++ core/src/main/java/com/killerchess/core/controllers/app/AppController.java
@@ -1,4 +1,11 @@
 package com.killerchess.core.controllers.app;
 
+import javafx.scene.control.Button;
+
 public class AppController {
+    private Button testButton;
+
+    public void handleTestButtonClicked() {
+        System.out.println("Test Button Clicked");
+    }
 }
