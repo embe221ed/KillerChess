7b7728eccda660c94c4a3406d5189c0ee9ca4287
Testowy  Uzytkownik <pitermarszalek@o2.pl>
2018-04-02 22:45:52 +0200

    added spring annotations in order to create tables id DB.

--- core/src/main/java/com/killerchess/core/game/Game.java
+++ core/src/main/java/com/killerchess/core/game/Game.java
@@ -1,4 +1,43 @@
 package com.killerchess.core.game;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "game")
 public class Game {
+
+    private int id;
+    private String host;
+    private String guest;
+
+    @Id
+    @Column(name = "game_id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Column(name = "host")
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    @Column(name = "guest")
+    public String getGuest() {
+        return guest;
+    }
+
+    public void setGuest(String guest) {
+        this.guest = guest;
+    }
 }
--- core/src/main/java/com/killerchess/core/game/GameState.java
+++ core/src/main/java/com/killerchess/core/game/GameState.java
@@ -1,4 +1,48 @@
 package com.killerchess.core.game;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.JoinColumn;
+
+@Entity
+@Table(name = "game_state")
 public class GameState {
+
+    private String state;
+    private int stateNumber;
+    private Game game;
+
+    @Column(name = "state")
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    @Column(name = "state_number")
+    public int getStateNumber() {
+        return stateNumber;
+    }
+
+    public void setStateNumber(int stateNumber) {
+        this.stateNumber = stateNumber;
+    }
+
+    @Id
+    @JoinColumn(name = "game_id")
+    public Game getGame() {
+        return game;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
 }
