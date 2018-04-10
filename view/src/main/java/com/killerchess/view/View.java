20825c7af8463085d9f81295328adc8c4f9de3e3
embchnk <baniak996@gmail.com>
2018-04-10 23:16:36 +0200

    Add logging and registration JavaFX GUI

--- view/src/main/java/com/killerchess/view/View.java
+++ view/src/main/java/com/killerchess/view/View.java
@@ -10,15 +10,37 @@ import javafx.stage.Stage;
 
 public class View extends Application {
 
+    private static View instance;
+    private Stage stage;
+
+    public View() {
+        instance = this;
+    }
+
     public static void main(String[] args) {
         launch(args);
     }
 
+    public static View getInstance() {
+        return instance;
+    }
+
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("logging/logging.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+        this.stage = primaryStage;
+        this.changeScene("logging/logging.fxml");
+    }
+
+    public void changeScene(String fxml) throws Exception {
+        Parent page = FXMLLoader.load(getClass().getResource(fxml));
+        Scene scene = stage.getScene();
+        if (scene == null) {
+            scene = new Scene(page);
+            stage.setScene(scene);
+        } else {
+            stage.getScene().setRoot(page);
+        }
+        stage.sizeToScene();
+        stage.show();
     }
 }
--- view/src/main/java/com/killerchess/view/logging/DummyClass.java
+++
@@ -1,5 +0,0 @@
-package com.killerchess.view.logging;
-
-public class DummyClass {
-    //    TODO remove after implementing real class. Class created just to git push purpose
-}
