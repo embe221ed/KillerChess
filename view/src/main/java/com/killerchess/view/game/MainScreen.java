0a55a27d3e4738ea2036ece97dec9b2ac49b88f4
rxrt8 <michal25kos@gmail.com>
2018-05-08 22:52:24 +0200

    Created mainScreen view

--- view/src/main/java/com/killerchess/view/game/MainScreen.java
+++ view/src/main/java/com/killerchess/view/game/MainScreen.java
@@ -1,6 +1,6 @@
 package com.killerchess.view.game;
 
-public class DummyClass {
+public class MainScreen {
 //    TODO remove after implementing real class. Class created just to git push purpose
 }
 
