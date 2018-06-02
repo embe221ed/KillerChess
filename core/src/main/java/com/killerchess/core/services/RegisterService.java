e70b9586d569718d719a269661e95b868907022c
ppaula <palcowskapaula@gmail.com>
2018-06-02 19:23:35 +0200

    password validation improved and tested

--- core/src/main/java/com/killerchess/core/services/RegisterService.java
+++ core/src/main/java/com/killerchess/core/services/RegisterService.java
@@ -8,6 +8,9 @@ public class RegisterService {
 
     //TODO Usually, we will contact via service with repository. It's example.
     public Boolean isValidUser(UserDTO userDTO) {
-        return userDTO.getPassword().length() > 8 && !userDTO.getPassword().equals(userDTO.getUsername());
+        String password = userDTO.getPassword();
+        return !password.toLowerCase().equals(userDTO.getUsername().toLowerCase())
+                && password.matches("^(?=.*\\d)(?=.*[a-z])(?=.*[\\!\\@\\#\\$\\%\\^\\&\\*\\(\\)\\_\\+\\-\\=])" +
+                "(?=.*[A-Z])(?!.*\\s).{8,}$") && password.length() <= 25;
     }
 }
