8296a15a14509e31d0f3ad05a99bf23c2d0873ed
ppaula <palcowskapaula@gmail.com>
2018-06-02 15:45:19 +0200

    VERTICAL scenario added

--- core/src/main/java/com/killerchess/core/chessboard/scenarios/GameScenariosEnum.java
+++ core/src/main/java/com/killerchess/core/chessboard/scenarios/GameScenariosEnum.java
@@ -1,7 +1,7 @@
 package com.killerchess.core.chessboard.scenarios;
 
 public enum GameScenariosEnum {
-    DEFAULT, NO_PAWN, NO_QUEEN, MIXED;
+    DEFAULT, NO_PAWN, NO_QUEEN, MIXED, VERTICAL;
 
     public String getJsonBOard() {
         switch (this) {
@@ -13,6 +13,8 @@ public enum GameScenariosEnum {
                 return "{\"1\":[\"RW\",\"HW\",\"BW\",\"XX\",\"KW\",\"BW\",\"HW\",\"RW\"],\"2\":[\"PW\",\"PW\",\"PW\",\"PW\",\"PW\",\"PW\",\"PW\",\"PW\"],\"3\":[\"XX\",\"XX\",\"XX\",\"XX\",\"XX\",\"XX\",\"XX\",\"XX\"],\"4\":[\"XX\",\"XX\",\"XX\",\"XX\",\"XX\",\"XX\",\"XX\",\"XX\"],\"5\":[\"XX\",\"XX\",\"XX\",\"XX\",\"XX\",\"XX\",\"XX\",\"XX\"],\"6\":[\"XX\",\"XX\",\"XX\",\"XX\",\"XX\",\"XX\",\"XX\",\"XX\"],\"7\":[\"PB\",\"PB\",\"PB\",\"PB\",\"PB\",\"PB\",\"PB\",\"PB\"],\"8\":[\"RB\",\"HB\",\"BB\",\"XX\",\"KB\",\"BB\",\"HB\",\"RB\"]}";
             case MIXED:
                 return "{\"1\":[\"BW\",\"HW\",\"BW\",\"HW\",\"KW\",\"RW\",\"QW\",\"RW\"],\"2\":[\"PW\",\"PW\",\"PW\",\"PW\",\"PW\",\"PW\",\"PW\",\"PW\"],\"3\":[\"XX\",\"XX\",\"XX\",\"XX\",\"XX\",\"XX\",\"XX\",\"XX\"],\"4\":[\"XX\",\"XX\",\"XX\",\"XX\",\"XX\",\"XX\",\"XX\",\"XX\"],\"5\":[\"XX\",\"XX\",\"XX\",\"XX\",\"XX\",\"XX\",\"XX\",\"XX\"],\"6\":[\"XX\",\"XX\",\"XX\",\"XX\",\"XX\",\"XX\",\"XX\",\"XX\"],\"7\":[\"PB\",\"PB\",\"PB\",\"PB\",\"PB\",\"PB\",\"PB\",\"PB\"],\"8\":[\"BB\",\"HB\",\"BB\",\"HB\",\"KB\",\"RB\",\"QB\",\"RB\"]}";
+            case VERTICAL:
+                return "{\"1\":[\"RW\",\"XX\",\"XX\",\"XX\",\"XX\",\"XX\",\"XX\",\"RB\"],\"2\":[\"HW\",\"XX\",\"XX\",\"XX\",\"XX\",\"XX\",\"XX\",\"HB\"],\"3\":[\"BW\",\"XX\",\"XX\",\"XX\",\"XX\",\"XX\",\"XX\",\"BB\"],\"4\":[\"QW\",\"XX\",\"XX\",\"XX\",\"XX\",\"XX\",\"XX\",\"KB\"],\"5\":[\"KW\",\"XX\",\"XX\",\"XX\",\"XX\",\"XX\",\"XX\",\"QB\"],\"6\":[\"BW\",\"XX\",\"XX\",\"XX\",\"XX\",\"XX\",\"XX\",\"BB\"],\"7\":[\"HW\",\"XX\",\"XX\",\"XX\",\"XX\",\"XX\",\"XX\",\"HB\"],\"8\":[\"RW\",\"XX\",\"XX\",\"XX\",\"XX\",\"XX\",\"XX\",\"RB\"]}";
         }
         return null;
     }
