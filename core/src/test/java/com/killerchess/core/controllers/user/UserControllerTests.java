85fe9215eb7d77f3ae24aa1708342d0a2ea45046
ppaula <palcowskapaula@gmail.com>
2018-06-06 12:04:50 +0200

    added registration tests

--- core/src/test/java/com/killerchess/core/controllers/user/UserControllerTests.java
+++ core/src/test/java/com/killerchess/core/controllers/user/UserControllerTests.java
@@ -6,11 +6,13 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -20,13 +22,15 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @WebAppConfiguration
 @ActiveProfiles("test")
+@Transactional
+@Rollback
 public class UserControllerTests {
 
     @Autowired
     private MockMvc mvc;
 
     @Test
-    public void simpleSuccessTest() throws Exception {
+    public void whenRegistrationSuccess() throws Exception {
         mvc.perform(MockMvcRequestBuilders.post("/register")
                 .param("username", "jan")
                 .param("password", "Kowalsky9@"))
@@ -34,10 +38,23 @@ public class UserControllerTests {
     }
 
     @Test
-    public void simpleFailedTest() throws Exception {
+    public void whenRegistrationFailBecausePasswordIsWrong() throws Exception {
         mvc.perform(MockMvcRequestBuilders.post("/register")
-                .param("username", "jan")
-                .param("password", "wrongpassword"))
+                .param("username", "janek")
+                .param("password", "wrongPassword"))
+                .andExpect(status().is(406));
+    }
+
+    @Test
+    public void whenRegistrationFailBecauseLoginIsTaken() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.post("/register")
+                .param("username", "tomek")
+                .param("password", "Kowalsky9@"))
+                .andExpect(status().is(200));
+
+        mvc.perform(MockMvcRequestBuilders.post("/register")
+                .param("username", "tomek")
+                .param("password", "Kowalsky9@"))
                 .andExpect(status().is(406));
     }
 }
