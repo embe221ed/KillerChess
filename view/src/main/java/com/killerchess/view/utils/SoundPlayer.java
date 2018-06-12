84bf9f69b81ba4fdb30d5464d131ff4bdcaf3e1d
ppaula <palcowskapaula@gmail.com>
2018-06-12 23:04:08 +0200

    fix sounds

--- view/src/main/java/com/killerchess/view/utils/SoundPlayer.java
+++ view/src/main/java/com/killerchess/view/utils/SoundPlayer.java
@@ -4,53 +4,57 @@ import javax.sound.sampled.*;
 import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 
 public class SoundPlayer {
 
-    private String end_game = "view/sounds/end_game.wav";
-    private String start_game = "view/sounds/start_game.wav";
-    private String move = "view/sounds/move.wav";
-    private String click = "view/sounds/click.wav";
+    private final String end_game = "view/sounds/end_game.wav";
+    private final String start_game = "view/sounds/start_game.wav";
+    private final String move = "view/sounds/move.wav";
+    private final String click = "view/sounds/click.wav";
 
-    public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+    public static void main(String[] args) {
         SoundPlayer soundPlayer = new SoundPlayer();
+
         soundPlayer.playOnGameStart();
         soundPlayer.playOnGameEnd();
         soundPlayer.playOnChessmanClick();
         soundPlayer.playOnChessmanMove();
     }
 
-    private void playSound(String filePath) throws LineUnavailableException, IOException, UnsupportedAudioFileException {
+    private void playSound(String filePath) {
 
-        InputStream audioSrc = new FileInputStream(filePath);
-        InputStream bufferedIn = new BufferedInputStream(audioSrc);
-        AudioInputStream inputStream = AudioSystem.getAudioInputStream(bufferedIn);
-        Clip clip = AudioSystem.getClip();
-        clip.open(inputStream);
-        FloatControl volume = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
-        volume.setValue(volume.getMinimum() + (0.8f * (volume.getMaximum() - volume.getMinimum())));
-        clip.setFramePosition(0);
-        clip.start();
-        clip.loop(0);
-        while (clip.isRunning()) {
+        try {
+            AudioInputStream inputStream = AudioSystem.getAudioInputStream(new BufferedInputStream(new FileInputStream(filePath)));
+
+            Clip clip = AudioSystem.getClip();
+            clip.open(inputStream);
+            clip.setFramePosition(0);
+
+            clip.start();
+            while (clip.getFramePosition() != clip.getFrameLength()) {
+            }
+            clip.close();
+
+            inputStream.close();
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+            e.printStackTrace();
         }
-        inputStream.close();
+
     }
 
-    public void playOnChessmanMove() throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+    public void playOnChessmanMove() {
         playSound(move);
     }
 
-    public void playOnChessmanClick() throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+    public void playOnChessmanClick() {
         playSound(click);
     }
 
-    public void playOnGameStart() throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+    public void playOnGameStart() {
         playSound(start_game);
     }
 
-    public void playOnGameEnd() throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+    public void playOnGameEnd() {
         playSound(end_game);
     }
 
